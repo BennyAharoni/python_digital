8ba5e128baec36c6df298bddf61130145e2cd1c3
Binyamin Aharoni <bennyaharoni@gmail.com>
2020-09-11 16:04:11 +0300

    targil-1

--- Lesson1/targil2.py
+++ Lesson1/targil2.py
@@ -7,10 +7,7 @@ num2 = num * 10
 print("Your number is: " + str(num))
 num = int(input("Enter number: "))
 print("Alafim = " + str(num // 1000))
-num = num % 1000
-print("Meot = " + str(num // 100))
-num = num % 100
-print("Asarot = " + str(num // 10))
-num = num % 10
-print("Ahadot = " + str(num // 1))
+print("Meot = " + str((num % 1000) // 100))
+print("Asarot = " + str((num % 100) // 10))
+print("Ahadot = " + str((num % 10) // 1))
 num = num % 1000
