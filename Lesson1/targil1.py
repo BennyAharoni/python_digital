6e0c02af720c09a924960e13ad2205946444d777
Binyamin Aharoni <bennyaharoni@gmail.com>
2020-09-11 14:28:40 +0300

    targil1

--- Lesson1/targil1.py
+++ Lesson1/targil1.py
@@ -1,2 +1,21 @@
-print("Hello World")
-print("Excited!")
+a = 12
+b = 11.56
+c = "Benny"
+
+#This is Integer
+print(a)
+print(type(a))
+
+#This is Float
+print(b)
+print(type(b))
+
+#This is String
+print(c)
+print(type(c))
+
+print("Hello\nHow are you, " + c + "?")
+print("I am " + str(a) + " years old")
+
+mail = input("Enter you mail: ")
+print(mail)
