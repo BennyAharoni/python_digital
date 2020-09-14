9ae90a344c9655744b7e375a6e4a07dde0247303
Binyamin Aharoni <bennyaharoni@gmail.com>
2020-09-14 23:19:48 +0300

    targil-1

--- Lesson1/LAB6.py
+++ Lesson1/LAB6.py
@@ -1,16 +1,29 @@
 from time import sleep
 
-menu_id = input("Enter your chosen: ")
+menu_id = input("Menu:\n----------\n"
+                "1. Insert a number and ** it by 3.\n"
+                "2. Insert 4 IPs to a list and print it.\n"
+                "3. Insert 4 Entries of DNS and print it\n"
+                "4. Input a string and check if it is a palindrome.\n"
+                "Enter your choice: ")
+
 if menu_id == "1":
     number = int(input("Enter a number: "))
     print(str(number) + " powered by 3 is: " + str(number ** 3))
 elif menu_id == "2":
-    ip_list = ["1.1.1.1", "2.2.2.2", "3.3.3.3", "4.4.4.4"]
-    print(ip_list)
+    ip_list = []
+    ip_list.append(input("Enter new IP: "))
+    ip_list.append(input("Enter new IP: "))
+    ip_list.append(input("Enter new IP: "))
+    ip_list.append(input("Enter new IP: "))
+    print("The new list is:\n----------\n" + str(ip_list))
 elif menu_id == "3":
-    dns_dict = {"www.google.com": "192.168.2.2", "www.ynet.com": "2.2.2.2", "www.apple.com": "3.3.3.3",
-                "www.youtube.com": "4.4.4.4"}
-    print(dns_dict)
+    dns_dict = {}
+    dns_dict.update({input("Enter a URL: "): input("Enter IP: ")})
+    dns_dict.update({input("Enter a URL: "): input("Enter IP: ")})
+    dns_dict.update({input("Enter a URL: "): input("Enter IP: ")})
+    dns_dict.update({input("Enter a URL: "): input("Enter IP: ")})
+    print("The new dns_dict is:\n----------\n" + str(dns_dict))
 elif menu_id == "4":
     str = input("Enter a string: ")
     if str == str[::-1]:
@@ -19,6 +32,6 @@ elif menu_id == "4":
         print(str + " is not a palindrome")
 
 else:
-    print("Bye")
+    print("Enter 1-4 only!")
 
 print("\nIt was a nice game, Bye bye")
